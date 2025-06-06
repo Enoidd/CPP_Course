dbea4b1741b89b5af24513f4525d17e1eb0e9906
enoidd <giammaria.scardella#protonmail.com>
2025-06-06 15:53:31 +0200

    esercizio1_12

---
+++ Lezione 12 - Esercizio 1/esercizio1_12.cpp
@@ -0,0 +1,32 @@
+/* 
+
+Definisci una classe Studente con:
+
+    string nome, int eta, float media
+
+    Metodo void saluta()
+
+    Metodo void info()
+
+Nel main():
+
+    Chiedi all’utente quanti studenti inserire (massimo 30)
+
+    Usa un array di oggetti Studente
+
+    Per ogni studente:
+
+        Acquisisci i dati da input
+
+        Salva negli oggetti
+
+    Alla fine:
+
+        Stampa l’elenco degli studenti con info()
+
+        Stampa il nome dello studente con la media più alta
+
+        Stampa quanti studenti hanno una media ≥ 28
+
+*/
+
